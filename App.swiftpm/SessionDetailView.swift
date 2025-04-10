092adcce921989a3b4c5dd9cd97ee99eef4f6000
sokume <tsutomu.hayakawa@dena.com>
2025-04-10 09:26:59 +0900

    a

---
+++ App.swiftpm/SessionDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  App
+//
+//  Created by Tsutomu Hayakawa on 2025/04/10.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
