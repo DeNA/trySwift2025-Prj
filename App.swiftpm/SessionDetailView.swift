8f052e11d32612b5e2bba20550ca1d1c0a066d0d
sokume <tsutomu.hayakawa@dena.com>
2025-04-10 15:49:07 +0900

    mikaela, scroll view on SessionDetailView

--- App.swiftpm/SessionDetailView.swift
+++ App.swiftpm/SessionDetailView.swift
@@ -11,72 +11,74 @@ struct SessionDetailView: View {
     let session: Session
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text(session.title)
-                .font(.title)
-                .padding()
-                .multilineTextAlignment(.leading)
-            
-            HStack {
-                Text("登壇者:").padding()
-                Text(session.speakers?.compactMap(\.name).joined(separator: ", ") ?? "なし")
-                Spacer()
-            }
-            
-            // TODO: UIをきれいにしてください！！
-            if let speakers = session.speakers {
-                HStack {
-                    Text("登壇者の概要:")
-                    
-                    Text(speakers.compactMap(\.bio).joined(separator: "\n"))
-                }
-            }
-            
-            if let speakers = session.speakers {
-                HStack(spacing: 8) {
-                    Text("職業")
-                    Text(speakers.compactMap(\.jobTitle).joined(separator: ", "))
-                }
-            }
-            
-            HStack {
-                Text("会場:").padding()
-                Text(session.place)
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text(session.title)
+                    .font(.title)
+                    .padding()
+                    .multilineTextAlignment(.leading)
                 
-                Spacer()
-            }
-            if #available(iOS 17.0, *) {
-                if let summary = session.summary {
-                    Text("概要:").padding()
-                    Text(summary)
-                        .padding(.horizontal, 16.0)
-                } else if let description = session.description {
-                    Text("概要:").padding()
-                    Text(description)
-                        .padding(.horizontal, 16.0)
-                } else {
-                    ContentUnavailableView {
-                        Label("概要なし", systemImage: "exclamationmark.bubble.fill")
-                    } description: {
-                        Text("このセッションには概要がありません")
+                HStack {
+                    Text("登壇者:").padding()
+                    Text(session.speakers?.compactMap(\.name).joined(separator: ", ") ?? "なし")
+                    Spacer()
+                }
+                
+                // TODO: UIをきれいにしてください！！
+                if let speakers = session.speakers {
+                    HStack {
+                        Text("登壇者の概要:")
+                        
+                        Text(speakers.compactMap(\.bio).joined(separator: "\n"))
                     }
                 }
                 
-            } else {
+                if let speakers = session.speakers {
+                    HStack(spacing: 8) {
+                        Text("職業")
+                        Text(speakers.compactMap(\.jobTitle).joined(separator: ", "))
+                    }
+                }
+                
                 HStack {
-                    // Fallback on earlier versions
-                    Text("概要:").padding()
+                    Text("会場:").padding()
+                    Text(session.place)
+                    
+                    Spacer()
+                }
+                if #available(iOS 17.0, *) {
                     if let summary = session.summary {
+                        Text("概要:").padding()
                         Text(summary)
                             .padding(.horizontal, 16.0)
+                    } else if let description = session.description {
+                        Text("概要:").padding()
+                        Text(description)
+                            .padding(.horizontal, 16.0)
                     } else {
-                        Text("なし")
+                        ContentUnavailableView {
+                            Label("概要なし", systemImage: "exclamationmark.bubble.fill")
+                        } description: {
+                            Text("このセッションには概要がありません")
+                        }
                     }
                     
+                } else {
+                    HStack {
+                        // Fallback on earlier versions
+                        Text("概要:").padding()
+                        if let summary = session.summary {
+                            Text(summary)
+                                .padding(.horizontal, 16.0)
+                        } else {
+                            Text("なし")
+                        }
+                        
+                    }
                 }
+                
+                Spacer()
             }
-            
-            Spacer()
         }
     }
 }
