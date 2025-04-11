d8935f89f66bd6a7fec7b8250abeec1b2cbb08c4
sokume <tsutomu.hayakawa@dena.com>
2025-04-11 14:01:20 +0900

    Ushikoshi & uhooi

--- App.swiftpm/DropdownSelectableTimetableView.swift
+++ App.swiftpm/DropdownSelectableTimetableView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TimetableListItem: View {
     let session: Session
-
+    
     var body: some View {
         HStack{
             if let speakers = session.speakers {
@@ -41,7 +41,7 @@ struct TimetableListItem: View {
 
 struct TimetableList: View {
     let timetable: Timetable?
-
+    
     var body: some View {
         List {
             ForEach(timetable?.schedules ?? []) { schedule in
@@ -51,8 +51,8 @@ struct TimetableList: View {
                             SessionDetailView(session: session, schedule: schedule) // FIXME: イケてる引数にして
                         } label: {
                             TimetableListItem(session: session)
-                            .opacity(schedule.hasEnded ? 0.5 : 1)
-
+                                .opacity(schedule.hasEnded ? 0.5 : 1)
+                            
                         }
                     }
                 } header: {
@@ -79,55 +79,152 @@ struct DropdownSelectableTimetableView: View {
     
     var body: some View {
         NavigationStack {
+            
             TimetableList(timetable: selectedDayTimetable)
-            .onAppear {
-                var timetables: [Timetable] = []
-                let decoder = JSONDecoder()
-                
-                for day in 1...3 {
-                    do {
-                        let timetable = try decoder.decode(
-                            Timetable.self,
-                            from: Data(
-                                contentsOf: Bundle.main.url(forResource: "2025-day\(day)", withExtension: "json")!
+                .onAppear {
+                    var timetables: [Timetable] = []
+                    let decoder = JSONDecoder()
+                    
+                    for day in 1...3 {
+                        do {
+                            let timetable = try decoder.decode(
+                                Timetable.self,
+                                from: Data(
+                                    contentsOf: Bundle.main.url(forResource: "2025-day\(day)", withExtension: "json")!
+                                )
                             )
-                        )
-                        timetables.append(timetable)
-                    } catch {
-                        //
-                        print("Error: \(error)")
-                    }
-                }
-                
-                self.timetables = timetables
-                selectedDayTimetable = timetables[selectedDayNumber]
-            }
-            .sheet(isPresented: $isLicenseViewPresented) {
-                NavigationStack {
-                    InfoView()
-                }
-            }
-            .onChange(of: selectedDayNumber) { newValue in
-                selectedDayTimetable = timetables[newValue]
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Picker("Day \(selectedDayNumber)", selection: $selectedDayNumber) {
-                        ForEach(Array(zip(timetables.indices, timetables)), id: \.0) { index, timetable in
-                            Text(timetable.title)
-                                .foregroundColor(Color(UIColor.systemBlue))
-                                .tag(index)
+                            timetables.append(timetable)
+                        } catch {
+                            //
+                            print("Error: \(error)")
                         }
                     }
-                    .pickerStyle(.menu)
+                    
+                    self.timetables = timetables
+                    selectedDayTimetable = timetables[selectedDayNumber]
                 }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: showLicenseView) {
-                        Image(systemName: "info.circle")
+                .sheet(isPresented: $isLicenseViewPresented) {
+                    NavigationStack {
+                        InfoView()
                     }
                 }
-            }
+                .onChange(of: selectedDayNumber) { newValue in
+                    selectedDayTimetable = timetables[newValue]
+                }
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        ZStack {
+                            HStack {
+                                Text("")
+                                    .foregroundColor(.accentColor)
+                                
+                                Spacer()
+                                
+                                Image(systemName: "chevron.up.chevron.down")
+                                    .foregroundColor(.accentColor)
+                            }
+                            .padding(.trailing, -12)
+                            
+                            HStack {
+                                Picker(
+                                    "",
+                                    selection: $selectedDayNumber
+                                ) {
+                                    ForEach(Array(zip(timetables.indices, timetables)), id: \.0) { index, timetable in
+                                        Text(timetable.title)
+                                            .foregroundColor(Color(UIColor.systemBlue))
+                                            .tag(index)
+                                    }
+                                }
+                                .pickerStyle(.menu)
+                                .foregroundStyle(.clear)
+                            }
+                        }
+//                        ZStack {
+//                            HStack {
+//                                Text("Day \(selectedDayNumber)")
+//                                    .foregroundColor(.accentColor)
+//                                
+//                                Image(systemName: "chevron.up.chevron.down")
+//                                    .foregroundColor(.accentColor)
+//                            }
+//                            
+//                            HStack {
+//                                Picker(
+//                                    "",
+//                                    selection: $selectedDayNumber
+//                                ) {
+//                                    ForEach(Array(zip(timetables.indices, timetables)), id: \.0) { index, timetable in
+//                                        Text(timetable.title)
+//                                            .foregroundColor(Color(UIColor.systemBlue))
+//                                            .tag(index)
+//                                    }
+//                                }
+//                                .pickerStyle(.menu)
+//                                .foregroundStyle(.clear)
+//                            }
+//                        }
+
+//                        HStack {
+//                            Text("Day \(selectedDayNumber)")
+//                                .foregroundColor(.accentColor)
+//                            
+//                            Spacer()
+//                            
+//                            Image(systemName: "chevron.up.chevron.down")
+//                                .foregroundColor(.accentColor)
+//                        }
+//                        .frame(maxWidth: .infinity)
+//                        .overlay {
+//                            Picker(
+//                                selection: $selectedDayNumber,
+//                                label: Text(" ").frame(maxWidth: .infinity)
+//                            ) {
+//                                ForEach(Array(zip(timetables.indices, timetables)), id: \.0) { index, timetable in
+//                                    Text(timetable.title)
+//                                        .foregroundColor(Color(UIColor.systemBlue))
+//                                        .tag(index)
+//                                }
+//                            }
+//                            .pickerStyle(.menu)
+//                            .foregroundStyle(.clear)
+//                        }
+                        //                    .overlay {
+                        //                        Picker(
+                        //                            "",
+                        //                            selection: $selectedDayNumber
+                        //                        ) {
+                        //                            ForEach(Array(zip(timetables.indices, timetables)), id: \.0) { index, timetable in
+                        //                                Text(timetable.title)
+                        //                                    .foregroundColor(Color(UIColor.systemBlue))
+                        //                                    .tag(index)
+                        //                            }
+                        //                        }
+                        //                        .pickerStyle(.menu)
+                    }
+                    
+                    
+                    //                    Picker(
+                    //                        "Day \(selectedDayNumber)",
+                    //                        systemImage: "chevron.up.chevron.down",
+                    //                        selection: $selectedDayNumber
+                    //                    ) {
+                    //                        ForEach(Array(zip(timetables.indices, timetables)), id: \.0) { index, timetable in
+                    //                            Text(timetable.title)
+                    //                                .foregroundColor(Color(UIColor.systemBlue))
+                    //                                .tag(index)
+                    //                        }
+                    //                    }
+                    //                    .pickerStyle(.menu)
+                    //                    .labelStyle(.titleAndIcon)
+                    //                }
+                    
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button(action: showLicenseView) {
+                            Image(systemName: "info.circle")
+                        }
+                    }
+                }
         }
     }
 }
